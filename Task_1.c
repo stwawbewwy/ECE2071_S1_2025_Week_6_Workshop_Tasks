061da7d212e2b50fd100df17e5409ae85007302a
stwawbewwy <asauce340@gmail.com>
2025-04-07 12:22:39 +0800

    Revert "gay"
    This reverts commit 4a50336e52aad05b96f063e02e7cee24591e4990.

--- Task_1.c
+++ Task_1.c
@@ -20,8 +20,6 @@ argument, only the filtered string must be printed onto the terminal.
 *******************************************************************************************************
 */
 
-// BALLS
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
